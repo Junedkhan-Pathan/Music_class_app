f022a3e82d8f2478b01a09544d9eb2fe72d0cd5f
logicrays <junedkhanpathan95@gmail.com>
2024-05-17 10:37:02 +0530

    Project upload

---
+++ src/types/course.interface.ts
@@ -0,0 +1,11 @@
+export interface ICourse {
+    id: number;
+    title: string;
+    slug: string;
+    description: string;
+    price: number;
+    instructor: string;
+    isFeatured: boolean;
+    image:string;
+  }
+  
